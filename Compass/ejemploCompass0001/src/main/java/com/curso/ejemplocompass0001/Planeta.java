cbd234c60cb6e56af7c633f19d7186d0e7e2eef3
iconojdiazch <khepherer@gmail.com>
2018-10-27 13:57:11 +0200

    Añadido ejemploCompass0001

---
+++ Compass/ejemploCompass0001/src/main/java/com/curso/ejemplocompass0001/Planeta.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curso.ejemplocompass0001;
+
+import org.compass.annotations.Index;
+import org.compass.annotations.Searchable;
+import org.compass.annotations.SearchableId;
+import org.compass.annotations.SearchableProperty;
+
+@Searchable
+public class Planeta {
+
+    @SearchableId
+    private int codigo;
+    @SearchableProperty(index = Index.TOKENIZED)
+    private String nombre;
+    @SearchableProperty
+    private long diametro;
+    @SearchableProperty(index = Index.TOKENIZED)
+    private String tipo;
+    @SearchableProperty(index = Index.TOKENIZED)
+    private String significado;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public long getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(long diametro) {
+        this.diametro = diametro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSignificado() {
+        return significado;
+    }
+
+    public void setSignificado(String significado) {
+        this.significado = significado;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Planeta{" + "codigo=" + codigo + ", nombre=" + nombre + ", diametro=" + diametro + ", tipo=" + tipo + ", significado=" + significado + '}';
+    }
+}
---
+++ Compass/ejemploCompass0001/src/main/java/com/curso/ejemplocompass0001/Principal.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curso.ejemplocompass0001;
+
+import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import org.compass.core.Compass;
+import org.compass.core.CompassCallback;
+import org.compass.core.CompassCallbackWithoutResult;
+import org.compass.core.CompassException;
+import org.compass.core.CompassHits;
+import org.compass.core.CompassSession;
+import org.compass.core.CompassTemplate;
+import org.compass.core.CompassTransaction;
+import org.compass.core.Resource;
+import org.compass.core.config.CompassConfiguration;
+
+/**
+ *
+ * @author usuario
+ */
+public class Principal {
+
+    private static final Logger LOG = Logger.getLogger(Principal.class.getName());
+    private Compass compass;
+    private CompassTemplate compassTemplate;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.inicializarCompass();
+        p.indexarPlanetas(p.crearObjetosPlaneta());
+        p.buscarPlanetasUsandoCompassCallbackWithoutResult("nombre:* AND tipo:rocoso AND -marte");
+        p.buscarPlanetasUsandoCompassCallback("nombre:* AND tipo:rocoso AND -marte").forEach(out::println);
+        LOG.info("Buscando el planeta mercurio".toUpperCase());
+        Planeta mercurio = p.buscarPlanetaPorId(10);
+        System.out.println("::::::" + mercurio);
+        LOG.info("Actualizando el planeta mercurio".toUpperCase());
+        mercurio = p.actualizarPlanetaPorId(10);
+        System.out.println("::::::" + mercurio);
+        LOG.info("borrando el planeta mercurio".toUpperCase());
+        p.borrarPlanetaPorId(10);
+        p.cerrar();
+    }
+
+    private void inicializarCompass() {
+        LOG.info("Inicializando Compass".toUpperCase());
+        CompassConfiguration config = new CompassConfiguration().configure().addClass(Planeta.class);
+        compass = config.buildCompass();
+        compass.getSearchEngineIndexManager().deleteIndex();
+        compass.getSearchEngineIndexManager().createIndex();
+        compassTemplate = new CompassTemplate(compass);
+        LOG.info("Fin de la inicialización".toUpperCase());
+    }
+
+    private void cerrar() {
+        compass.close();
+        LOG.info("Compass cerrado".toUpperCase());
+    }
+
+    private List<Planeta> crearObjetosPlaneta() {
+        List<Planeta> planetas = new ArrayList<>();
+        Planeta planeta = new Planeta();
+        planeta.setCodigo(10);
+        planeta.setNombre("Mercurio");
+        planeta.setDiametro(4879);
+        planeta.setTipo("rocoso");
+        planeta.setSignificado("mensajero de los dioses");
+        planetas.add(planeta);
+        return planetas;
+    }
+
+    private List<Planeta> indexarPlanetas(List<Planeta> planetas) {
+        CompassSession session = compass.openSession();
+        CompassTransaction tx = session.beginTransaction();
+        planetas.forEach(session::save);
+        tx.commit();
+        session.close();
+        return planetas;
+    }
+
+    private void mostrar(CompassHits hits, int hitNumber) {
+        Planeta planeta = (Planeta) hits.data(hitNumber);
+        Resource resource = hits.resource(hitNumber);
+        System.out.println("ALIAS [" + resource.getAlias() + "] ID [" + planeta.getCodigo() + "] SCORE ["
+                + hits.score(hitNumber) + "]");
+        System.out.println("El planeta es:" + planeta);
+    }
+
+    private void buscarPlanetasUsandoCompassCallbackWithoutResult(String consulta) {
+
+        compassTemplate.execute(new CompassCallbackWithoutResult() {
+            @Override
+            protected void doInCompassWithoutResult(CompassSession session) throws CompassException {
+                CompassHits sugerencias = session.find(consulta);
+                System.out.println("Se han encontrado [" + sugerencias.getLength() + "] sugerencias para [" + consulta + "]");
+                System.out.println("Mostrando los datos encontrados...".toUpperCase());
+                for (int i = 0; i < sugerencias.getLength(); i++) {
+                    mostrar(sugerencias, i);
+                }
+                sugerencias.close();
+            }
+        });
+    }
+
+    private List<Planeta> buscarPlanetasUsandoCompassCallback(String consulta) {
+        return compassTemplate.execute(new CompassCallback<List<Planeta>>() {
+            @Override
+            public List<Planeta> doInCompass(CompassSession session) throws CompassException {
+                List<Planeta> r = new ArrayList<>();
+                CompassHits sugerencias = session.find(consulta);
+                System.out.println("Se han encontrado [" + sugerencias.getLength() + "] sugerencias para [" + consulta + "]");
+                for (int i = 0; i < sugerencias.getLength(); i++) {
+                    r.add((Planeta) sugerencias.data(i));
+                }
+                return r;
+            }
+        });
+    }
+
+    private Planeta buscarPlanetaPorId(Integer id) {
+        Planeta p;
+        CompassSession session = compass.openSession();
+        CompassTransaction tx = session.beginTransaction();
+        p = session.load(Planeta.class, id);
+        tx.commit();
+        session.close();        
+        return p;
+    }
+
+    private void borrarPlanetaPorId(Integer id) {
+        CompassSession session = compass.openSession();
+        CompassTransaction tx = session.beginTransaction();
+        session.delete(Planeta.class, id);
+        try {
+            session.load(Planeta.class, id);
+        } catch (CompassException e) {
+            System.out.println("::::::Error: " + e);
+        }
+        tx.commit();
+        session.close();
+    }
+
+    private Planeta actualizarPlanetaPorId(Integer id) {
+        Planeta p;
+        CompassSession session = compass.openSession();
+        CompassTransaction tx = session.beginTransaction();
+        p = session.load(Planeta.class, id);
+        p.setDiametro(0L);
+        session.save(p);
+        tx.commit();
+        session.close();        
+        return p;    }
+}
